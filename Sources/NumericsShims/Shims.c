d314e42a11646fa0ac12a2abd57c7e70bcfce7d2
Steve (Numerics) Canon <scanon@apple.com>
2019-06-24 11:54:10 -0400

    Add swift project header to NumericsShims source files.

--- Sources/NumericsShims/Shims.c
+++
@@ -1,1 +0,0 @@
-#include "NumericsShims.h"
--- Sources/NumericsShims/include/NumericsShims.h
+++ Sources/NumericsShims/include/NumericsShims.h
@@ -1,3 +1,15 @@
+//===--- NumericsShims.h --------------------------------------*- swift -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2019 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
 #define HEADER_SHIM static inline __attribute__((__always_inline__))
 
 // MARK: - math functions for float
