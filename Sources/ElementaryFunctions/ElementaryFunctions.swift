b97297c027d0332b4990d49306ac878f213667ae
Steve (Numerics) Canon <scanon@apple.com>
2019-06-30 17:16:18 -0400

    Reorder fns, add cross-links.

--- Sources/ElementaryFunctions/ElementaryFunctions.swift
+++ Sources/ElementaryFunctions/ElementaryFunctions.swift
@@ -29,65 +29,45 @@
 /// special functions, are provided on the RealFunctions protocol, which refines
 /// ElementaryFunctions.
 ///
+/// See Also:
+/// -
+/// - `RealFunctions`
+///
 /// [elfn]: http://en.wikipedia.org/wiki/Elementary_function
 public protocol ElementaryFunctions {
-  /// The [square root][wiki] of `x`.
+  /// The [exponential function][wiki] e^x whose base `e` is the base of the natural logarithm.
   ///
-  /// [wiki]: https://en.wikipedia.org/wiki/Square_root
-  static func sqrt(_ x: Self) -> Self
+  /// See also:
+  /// -
+  /// - `expm1()`
+  /// - `exp2()` (for types conforming to `RealFunctions`)
+  /// - `exp10()` (for types conforming to `RealFunctions`)
+  ///
+  /// [wiki]: https://en.wikipedia.org/wiki/Exponential_function
+  static func exp(_ x: Self) -> Self
   
-  /// The [cosine][wiki] of `x`.
+  /// exp(x) - 1, computed in such a way as to maintain accuracy for small x.
   ///
-  /// For real types, `x` may be interpreted as an angle measured in radians.
+  /// When `x` is close to zero, the expression `.exp(x) - 1` suffers from catastrophic
+  /// cancellation and the result will not have full accuracy. The `.expm1(x)` function gives
+  /// you a means to address this problem.
   ///
-  /// [wiki]: https://en.wikipedia.org/wiki/Cosine
-  static func cos(_ x: Self) -> Self
-  
-  
-  /// The [sine][wiki] of `x`.
+  /// As an example, consider the expression `(x + 1)*exp(x) - 1`.  When `x` is smaller
+  /// than `.ulpOfOne`, this expression evaluates to `0.0`, when it should actually round to
+  /// `2*x`. We can get a full-accuracy result by using the following instead:
+  /// ```
+  /// let expm1x = .expm1(x)
+  /// return x*(expm1x + 1) + expm1x
+  /// ```
+  /// This re-written expression delivers an accurate result for all values of `x`, not just for
+  /// small values.
   ///
-  /// For real types, `x` may be interpreted as an angle measured in radians.
-  ///
-  /// [wiki]: https://en.wikipedia.org/wiki/Sine
-  static func sin(_ x: Self) -> Self
-  
-  /// The [tangent][wiki] of `x`.
-  ///
-  /// For real types, `x` may be interpreted as an angle measured in radians.
-  /// ```
-  ///           sin(x)
-  /// tan(x) = --------
-  ///           cos(x)
-  /// ```
-  /// [wiki]: https://en.wikipedia.org/wiki/Tangent
-  static func tan(_ x: Self) -> Self
-  
-  /// The [arccosine][wiki] (inverse cosine) of `x`.
-  ///
-  /// For real types, the result may be interpreted as an angle measured in radians.
-  /// ```
-  /// cos(acos(x)) ≅ x
-  /// ```
-  /// [wiki]: https://en.wikipedia.org/wiki/Inverse_trigonometric_functions
-  static func acos(_ x: Self) -> Self
-  
-  /// The [arcsine][wiki]  (inverse sine) of `x`.
-  ///
-  /// For real types, the result may be interpreted as an angle measured in radians.
-  /// ```
-  /// sin(asin(x)) ≅ x
-  /// ```
-  /// [wiki]: https://en.wikipedia.org/wiki/Inverse_trigonometric_functions
-  static func asin(_ x: Self) -> Self
-  
-  /// The [arctangent][wiki]  (inverse tangent) of `x`.
-  ///
-  /// For real types, the result may be interpreted as an angle measured in radians.
-  /// ```
-  /// tan(atan(x)) ≅ x
-  /// ```
-  /// [wiki]: https://en.wikipedia.org/wiki/Inverse_trigonometric_functions
-  static func atan(_ x: Self) -> Self
+  /// See also:
+  /// -
+  /// - `exp()`
+  /// - `exp2()` (for types conforming to `RealFunctions`)
+  /// - `exp10()` (for types conforming to `RealFunctions`)
+  static func expm1(_ x: Self) -> Self
   
   /// The [hyperbolic cosine][wiki] of `x`.
   /// ```
@@ -95,6 +75,13 @@ public protocol ElementaryFunctions {
   /// cosh(x) = ------------
   ///                2
   /// ```
+  ///
+  /// See also:
+  /// -
+  /// - `sinh()`
+  /// - `tanh()`
+  /// - `acosh()`
+  ///
   /// [wiki]: https://en.wikipedia.org/wiki/Hyperbolic_function
   static func cosh(_ x: Self) -> Self
   
@@ -104,6 +91,13 @@ public protocol ElementaryFunctions {
   /// sinh(x) = ------------
   ///                2
   /// ```
+  ///
+  /// See also:
+  /// -
+  /// - `cosh()`
+  /// - `tanh()`
+  /// - `asinh()`
+  ///
   /// [wiki]: https://en.wikipedia.org/wiki/Hyperbolic_function
   static func sinh(_ x: Self) -> Self
   
@@ -113,13 +107,91 @@ public protocol ElementaryFunctions {
   /// tanh(x) = ---------
   ///            cosh(x)
   /// ```
+  ///
+  /// See also:
+  /// -
+  /// - `cosh()`
+  /// - `sinhh()`
+  /// - `atanh()`
+  ///
   /// [wiki]: https://en.wikipedia.org/wiki/Hyperbolic_function
   static func tanh(_ x: Self) -> Self
+  
+  /// The [cosine][wiki] of `x`.
+  ///
+  /// For real types, `x` may be interpreted as an angle measured in radians.
+  ///
+  /// See also:
+  /// -
+  /// - `sin()`
+  /// - `tan()`
+  /// - `acos()`
+  ///
+  /// [wiki]: https://en.wikipedia.org/wiki/Cosine
+  static func cos(_ x: Self) -> Self
+  
+  
+  /// The [sine][wiki] of `x`.
+  ///
+  /// For real types, `x` may be interpreted as an angle measured in radians.
+  ///
+  /// See also:
+  /// -
+  /// - `cos()`
+  /// - `tan()`
+  /// - `asin()`
+  ///
+  /// [wiki]: https://en.wikipedia.org/wiki/Sine
+  static func sin(_ x: Self) -> Self
+  
+  /// The [tangent][wiki] of `x`.
+  ///
+  /// For real types, `x` may be interpreted as an angle measured in radians.
+  ///
+  /// See also:
+  /// -
+  /// - `cos()`
+  /// - `sin()`
+  /// - `atan()`
+  /// - `atan2(y:x:)` (for types conforming to `RealFunctions`)
+  /// ```
+  ///           sin(x)
+  /// tan(x) = --------
+  ///           cos(x)
+  /// ```
+  /// [wiki]: https://en.wikipedia.org/wiki/Tangent
+  static func tan(_ x: Self) -> Self
+  
+  /// The [natural logarithm][wiki] of `x`.
+  ///
+  /// See also:
+  /// -
+  /// - `log1p()`
+  /// - `log2()` (for types conforming to `RealFunctions`)
+  /// - `log10()` (for types conforming to `RealFunctions`)
+  ///
+  /// [wiki]: https://en.wikipedia.org/wiki/Logarithm
+  static func log(_ x: Self) -> Self
+  
+  /// log(1 + x), computed in such a way as to maintain accuracy for small x.
+  ///
+  /// See also:
+  /// -
+  /// - `log()`
+  /// - `log2()` (for types conforming to `RealFunctions`)
+  /// - `log10()` (for types conforming to `RealFunctions`)
+  static func log1p(_ x: Self) -> Self
   
   /// The [inverse hyperbolic cosine][wiki] of `x`.
   /// ```
   /// cosh(acosh(x)) ≅ x
   /// ```
+  /// See also:
+  /// -
+  /// - `asinh()`
+  /// - `atanh()`
+  /// - `cosh()`
+  ///
   /// [wiki]: https://en.wikipedia.org/wiki/Inverse_hyperbolic_function
   static func acosh(_ x: Self) -> Self
   
@@ -127,6 +199,12 @@ public protocol ElementaryFunctions {
   /// ```
   /// sinh(asinh(x)) ≅ x
   /// ```
+  /// See also:
+  /// -
+  /// - `acosh()`
+  /// - `atanh()`
+  /// - `sinh()`
+  ///
   /// [wiki]: https://en.wikipedia.org/wiki/Inverse_hyperbolic_function
   static func asinh(_ x: Self) -> Self
   
@@ -134,32 +212,96 @@ public protocol ElementaryFunctions {
   /// ```
   /// tanh(atanh(x)) ≅ x
   /// ```
+  /// See also:
+  /// -
+  /// - `acosh()`
+  /// - `asinh()`
+  /// - `tanh()`
+  ///
   /// [wiki]: https://en.wikipedia.org/wiki/Inverse_hyperbolic_function
   static func atanh(_ x: Self) -> Self
   
-  /// The [exponential function][wiki] e^x whose base `e` is the base of the natural logarithm.
+  /// The [arccosine][wiki] (inverse cosine) of `x`.
   ///
-  /// [wiki]: https://en.wikipedia.org/wiki/Exponential_function
-  static func exp(_ x: Self) -> Self
-  
-  /// exp(x) - 1, computed in such a way as to maintain accuracy for small x.
-  static func expm1(_ x: Self) -> Self
-  
-  /// The natural [logarithm][wiki] of `x`.
+  /// For real types, the result may be interpreted as an angle measured in radians.
+  /// ```
+  /// cos(acos(x)) ≅ x
+  /// ```
+  /// See also:
+  /// -
+  /// - `asin()`
+  /// - `atan()`
+  /// - `cos()`
   ///
-  /// [wiki]: https://en.wikipedia.org/wiki/Logarithm
-  static func log(_ x: Self) -> Self
+  /// [wiki]: https://en.wikipedia.org/wiki/Inverse_trigonometric_functions
+  static func acos(_ x: Self) -> Self
   
-  /// log(1 + x), computed in such a way as to maintain accuracy for small x.
-  static func log1p(_ x: Self) -> Self
+  /// The [arcsine][wiki]  (inverse sine) of `x`.
+  ///
+  /// For real types, the result may be interpreted as an angle measured in radians.
+  /// ```
+  /// sin(asin(x)) ≅ x
+  /// ```
+  /// See also:
+  /// -
+  /// - `acos()`
+  /// - `atan()`
+  /// - `sin()`
+  ///
+  /// [wiki]: https://en.wikipedia.org/wiki/Inverse_trigonometric_functions
+  static func asin(_ x: Self) -> Self
+  
+  /// The [arctangent][wiki]  (inverse tangent) of `x`.
+  ///
+  /// For real types, the result may be interpreted as an angle measured in radians.
+  /// ```
+  /// tan(atan(x)) ≅ x
+  /// ```
+  /// See also:
+  /// -
+  /// - `acos()`
+  /// - `asin()`
+  /// - `atan2()` (for types conforming to `RealFunctions`)
+  /// - `tan()`
+  ///
+  /// [wiki]: https://en.wikipedia.org/wiki/Inverse_trigonometric_functions
+  static func atan(_ x: Self) -> Self
   
   /// exp(y * log(x)) computed with additional internal precision.
+  ///
+  /// See also:
+  /// -
+  /// - `sqrt()`
+  /// - `root()`
+  ///
   static func pow(_ x: Self, _ y: Self) -> Self
   
   /// `x` raised to the nth power.
+  ///
+  /// See also:
+  /// -
+  /// - `sqrt()`
+  /// - `root()`
+  ///
   static func pow(_ x: Self, _ n: Int) -> Self
   
+  /// The [square root][wiki] of `x`.
+  ///
+  /// See also:
+  /// -
+  /// - `pow()`
+  /// - `root()`
+  ///
+  /// [wiki]: https://en.wikipedia.org/wiki/Square_root
+  static func sqrt(_ x: Self) -> Self
+  
   /// The nth root of `x`.
+  ///
+  /// See also:
+  /// -
+  /// - `pow()`
+  /// - `sqrt()`
+  ///
   static func root(_ x: Self, _ n: Int) -> Self
 }
 
