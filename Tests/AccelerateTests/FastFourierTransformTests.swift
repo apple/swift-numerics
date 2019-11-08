937478fec02476694d69d09e722250de70f42a5a
Susan Cheng <>
2019-11-08 17:27:01 +0800

    cover all the cases

--- Tests/AccelerateTests/FastFourierTransformTests.swift
+++ Tests/AccelerateTests/FastFourierTransformTests.swift
@@ -50,13 +50,127 @@ final class FastFourierTransformTests: XCTestCase {
     
     let accuracy = 0.00000001
     
-    func test_vDSP_fft_zrop() {
+    func test_vDSP_fft_zrop_2() {
+        
+        let in_real: [Double] = [1, 2]
+        let in_imag: [Double] = [0, 0]
+        
+        var out_real: [Double] = Array(repeating: 0, count: 1)
+        var out_imag: [Double] = Array(repeating: 0, count: 1)
+        
+        vDSP_fft_zrop(1, in_real, 1, &out_real, &out_imag, 1)
+        
+        var check = _FDFT(zip(in_real, in_imag).map(Complex.init))
+        
+        // we only have half length of frequency domain
+        check[0] = Complex(check[0].real, check[1].real)
+        
+        for i in 0..<1 {
+            XCTAssertEqual(check[i].real, out_real[i], accuracy: accuracy)
+            XCTAssertEqual(check[i].imaginary, out_imag[i], accuracy: accuracy)
+        }
+    }
+    
+    func test_vDSP_fft_zop_forward_2() {
+        
+        let in_real: [Double] = [1, 2]
+        let in_imag: [Double] = [3, 4]
+        
+        var out_real: [Double] = Array(repeating: 0, count: 2)
+        var out_imag: [Double] = Array(repeating: 0, count: 2)
+        
+        vDSP_fft_zop(1, in_real, in_imag, 1, &out_real, &out_imag, 1, .forward)
+        
+        let check = _FDFT(zip(in_real, in_imag).map(Complex.init))
+        
+        for i in 0..<2 {
+            XCTAssertEqual(check[i].real, out_real[i], accuracy: accuracy)
+            XCTAssertEqual(check[i].imaginary, out_imag[i], accuracy: accuracy)
+        }
+    }
+    
+    func test_vDSP_fft_zop_inverse_2() {
+        
+        let in_real: [Double] = [1, 2]
+        let in_imag: [Double] = [3, 4]
+        
+        var out_real: [Double] = Array(repeating: 0, count: 2)
+        var out_imag: [Double] = Array(repeating: 0, count: 2)
+        
+        vDSP_fft_zop(1, in_real, in_imag, 1, &out_real, &out_imag, 1, .inverse)
+        
+        let check = _IDFT(zip(in_real, in_imag).map(Complex.init))
+        
+        for i in 0..<2 {
+            XCTAssertEqual(check[i].real, out_real[i], accuracy: accuracy)
+            XCTAssertEqual(check[i].imaginary, out_imag[i], accuracy: accuracy)
+        }
+    }
+    
+    func test_vDSP_fft_zrop_4() {
+        
+        let in_real: [Double] = [1, 2, 3, 4]
+        let in_imag: [Double] = [0, 0, 0, 0]
+        
+        var out_real: [Double] = Array(repeating: 0, count: 2)
+        var out_imag: [Double] = Array(repeating: 0, count: 2)
+        
+        vDSP_fft_zrop(2, in_real, 1, &out_real, &out_imag, 1)
+        
+        var check = _FDFT(zip(in_real, in_imag).map(Complex.init))
+        
+        // we only have half length of frequency domain
+        check[0] = Complex(check[0].real, check[2].real)
+        
+        for i in 0..<2 {
+            XCTAssertEqual(check[i].real, out_real[i], accuracy: accuracy)
+            XCTAssertEqual(check[i].imaginary, out_imag[i], accuracy: accuracy)
+        }
+    }
+    
+    func test_vDSP_fft_zop_forward_4() {
+        
+        let in_real: [Double] = [1, 2, 3, 4]
+        let in_imag: [Double] = [5, 6, 7, 8]
+        
+        var out_real: [Double] = Array(repeating: 0, count: 4)
+        var out_imag: [Double] = Array(repeating: 0, count: 4)
+        
+        vDSP_fft_zop(2, in_real, in_imag, 1, &out_real, &out_imag, 1, .forward)
+        
+        let check = _FDFT(zip(in_real, in_imag).map(Complex.init))
+        
+        for i in 0..<4 {
+            XCTAssertEqual(check[i].real, out_real[i], accuracy: accuracy)
+            XCTAssertEqual(check[i].imaginary, out_imag[i], accuracy: accuracy)
+        }
+    }
+    
+    func test_vDSP_fft_zop_inverse_4() {
+        
+        let in_real: [Double] = [1, 2, 3, 4]
+        let in_imag: [Double] = [5, 6, 7, 8]
+        
+        var out_real: [Double] = Array(repeating: 0, count: 4)
+        var out_imag: [Double] = Array(repeating: 0, count: 4)
+        
+        vDSP_fft_zop(2, in_real, in_imag, 1, &out_real, &out_imag, 1, .inverse)
+        
+        let check = _IDFT(zip(in_real, in_imag).map(Complex.init))
+        
+        for i in 0..<4 {
+            XCTAssertEqual(check[i].real, out_real[i], accuracy: accuracy)
+            XCTAssertEqual(check[i].imaginary, out_imag[i], accuracy: accuracy)
+        }
+    }
+    
+    func test_vDSP_fft_zrop_8() {
         
         let in_real: [Double] = [1, 2, 3, 4, 5, 6, 7, 8]
         let in_imag: [Double] = [0, 0, 0, 0, 0, 0, 0, 0]
         
-        var out_real: [Double] = Array(repeating: 0, count: 8)
-        var out_imag: [Double] = Array(repeating: 0, count: 8)
+        var out_real: [Double] = Array(repeating: 0, count: 4)
+        var out_imag: [Double] = Array(repeating: 0, count: 4)
         
         vDSP_fft_zrop(3, in_real, 1, &out_real, &out_imag, 1)
         
@@ -71,7 +185,7 @@ final class FastFourierTransformTests: XCTestCase {
         }
     }
     
-    func test_vDSP_fft_zop_forward() {
+    func test_vDSP_fft_zop_forward_8() {
         
         let in_real: [Double] = [1, 2, 3, 4, 5, 6, 7, 8]
         let in_imag: [Double] = [9, 10, 11, 12, 13, 14, 15, 16]
@@ -89,7 +203,7 @@ final class FastFourierTransformTests: XCTestCase {
         }
     }
     
-    func test_vDSP_fft_zop_inverse() {
+    func test_vDSP_fft_zop_inverse_8() {
         
         let in_real: [Double] = [1, 2, 3, 4, 5, 6, 7, 8]
         let in_imag: [Double] = [9, 10, 11, 12, 13, 14, 15, 16]
@@ -102,6 +216,63 @@ final class FastFourierTransformTests: XCTestCase {
         let check = _IDFT(zip(in_real, in_imag).map(Complex.init))
         
         for i in 0..<8 {
+            XCTAssertEqual(check[i].real, out_real[i], accuracy: accuracy)
+            XCTAssertEqual(check[i].imaginary, out_imag[i], accuracy: accuracy)
+        }
+    }
+    
+    func test_vDSP_fft_zrop_16() {
+        
+        let in_real: [Double] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        let in_imag: [Double] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        
+        var out_real: [Double] = Array(repeating: 0, count: 8)
+        var out_imag: [Double] = Array(repeating: 0, count: 8)
+        
+        vDSP_fft_zrop(4, in_real, 1, &out_real, &out_imag, 1)
+        
+        var check = _FDFT(zip(in_real, in_imag).map(Complex.init))
+        
+        // we only have half length of frequency domain
+        check[0] = Complex(check[0].real, check[8].real)
+        
+        for i in 0..<8 {
+            XCTAssertEqual(check[i].real, out_real[i], accuracy: accuracy)
+            XCTAssertEqual(check[i].imaginary, out_imag[i], accuracy: accuracy)
+        }
+    }
+    
+    func test_vDSP_fft_zop_forward_16() {
+        
+        let in_real: [Double] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        let in_imag: [Double] = [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+        
+        var out_real: [Double] = Array(repeating: 0, count: 16)
+        var out_imag: [Double] = Array(repeating: 0, count: 16)
+        
+        vDSP_fft_zop(4, in_real, in_imag, 1, &out_real, &out_imag, 1, .forward)
+        
+        let check = _FDFT(zip(in_real, in_imag).map(Complex.init))
+        
+        for i in 0..<16 {
+            XCTAssertEqual(check[i].real, out_real[i], accuracy: accuracy)
+            XCTAssertEqual(check[i].imaginary, out_imag[i], accuracy: accuracy)
+        }
+    }
+    
+    func test_vDSP_fft_zop_inverse_16() {
+        
+        let in_real: [Double] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        let in_imag: [Double] = [17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+        
+        var out_real: [Double] = Array(repeating: 0, count: 16)
+        var out_imag: [Double] = Array(repeating: 0, count: 16)
+        
+        vDSP_fft_zop(4, in_real, in_imag, 1, &out_real, &out_imag, 1, .inverse)
+        
+        let check = _IDFT(zip(in_real, in_imag).map(Complex.init))
+        
+        for i in 0..<16 {
             XCTAssertEqual(check[i].real, out_real[i], accuracy: accuracy)
             XCTAssertEqual(check[i].imaginary, out_imag[i], accuracy: accuracy)
         }
