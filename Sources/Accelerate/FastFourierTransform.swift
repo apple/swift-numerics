fada96636142737544ec2d8e814fe7f1c6758569
Susan Cheng <>
2019-11-08 18:18:42 +0800

    in-place real to complex fft

--- Sources/Accelerate/FastFourierTransform.swift
+++ Sources/Accelerate/FastFourierTransform.swift
@@ -34,7 +34,68 @@ public enum FFTDirection {
 public func vDSP_fft_zrop<T: Real & BinaryFloatingPoint>(_ log2N: Int, _ in_real: UnsafePointer<T>, _ in_imag: UnsafePointer<T>, _ in_stride: Int, _ out_real: UnsafeMutablePointer<T>, _ out_imag: UnsafeMutablePointer<T>, _ out_stride: Int, _ direction: FFTDirection) {
     switch direction {
     case .forward: vDSP_fft_zrop_forward_imp(log2N, in_real, in_imag, in_stride, out_real, out_imag, out_stride)
-    case .inverse: vDSP_fft_zrop_inverse_imp(log2N, in_imag, in_real, in_stride, out_imag, out_real, out_stride)
+    case .inverse: vDSP_fft_zrop_inverse_imp(log2N, in_real, in_imag, in_stride, out_real, out_imag, out_stride)
+    }
+}
+
+@inlinable
+@inline(__always)
+func vDSP_fft_zrop_forward_twiddling_imp<T: Real & BinaryFloatingPoint>(_ log2N: Int, _ real: UnsafeMutablePointer<T>, _ imag: UnsafeMutablePointer<T>, _ stride: Int) {
+    
+    // http://www.katjaas.nl/realFFT/realFFT2.html
+    
+    let length = 1 << log2N
+    let half = length >> 1
+    let fourth = length >> 2
+    
+    let _stride = half * stride
+    var op_r = real
+    var op_i = imag
+    var oph_r = real + _stride
+    var oph_i = imag + _stride
+    
+    let tr = op_r.pointee
+    let ti = op_i.pointee
+    op_r.pointee = tr + ti
+    op_i.pointee = tr - ti
+    
+    let opf_i = imag + fourth * stride
+    opf_i.pointee = -opf_i.pointee
+    
+    let angle = -T.pi / T(half)
+    let _cos = T.cos(angle)
+    let _sin = T.sin(angle)
+    var _cos1 = _cos
+    var _sin1 = _sin
+    for _ in 1..<fourth {
+        
+        op_r += stride
+        op_i += stride
+        oph_r -= stride
+        oph_i -= stride
+        
+        let or = op_r.pointee
+        let oi = op_i.pointee
+        let ohr = oph_r.pointee
+        let ohi = oph_i.pointee
+        
+        let evenreal = or + ohr
+        let evenim = oi - ohi
+        let oddreal = oi + ohi
+        let oddim = ohr - or
+        
+        let _r = oddreal * _cos1 - oddim * _sin1
+        let _i = oddreal * _sin1 + oddim * _cos1
+        
+        op_r.pointee = 0.5 * (evenreal + _r)
+        op_i.pointee = 0.5 * (_i + evenim)
+        oph_r.pointee = 0.5 * (evenreal - _r)
+        oph_i.pointee = 0.5 * (_i - evenim)
+        
+        let _c1 = _cos * _cos1 - _sin * _sin1
+        let _s1 = _cos * _sin1 + _sin * _cos1
+        _cos1 = _c1
+        _sin1 = _s1
     }
 }
 
@@ -56,63 +117,8 @@ func vDSP_fft_zrop_forward_imp<T: Real & BinaryFloatingPoint>(_ log2N: Int, _ in
         vDSP_fft_zrop_forward_8(in_real, in_imag, in_stride, out_real, out_imag, out_stride)
         
     default:
-        let length = 1 << log2N
-        let half = length >> 1
-        let fourth = length >> 2
-        
         vDSP_fft_zop_imp(log2N - 1, in_real, in_imag, in_stride, out_real, out_imag, out_stride)
-        
-        // http://www.katjaas.nl/realFFT/realFFT2.html
-        
-        let _stride = half * out_stride
-        var op_r = out_real
-        var op_i = out_imag
-        var oph_r = out_real + _stride
-        var oph_i = out_imag + _stride
-        
-        let tr = op_r.pointee
-        let ti = op_i.pointee
-        op_r.pointee = tr + ti
-        op_i.pointee = tr - ti
-        
-        let opf_i = out_imag + fourth * out_stride
-        opf_i.pointee = -opf_i.pointee
-        
-        let angle = -T.pi / T(half)
-        let _cos = T.cos(angle)
-        let _sin = T.sin(angle)
-        var _cos1 = _cos
-        var _sin1 = _sin
-        for _ in 1..<fourth {
-            
-            op_r += out_stride
-            op_i += out_stride
-            oph_r -= out_stride
-            oph_i -= out_stride
-            
-            let or = op_r.pointee
-            let oi = op_i.pointee
-            let ohr = oph_r.pointee
-            let ohi = oph_i.pointee
-            
-            let evenreal = or + ohr
-            let evenim = oi - ohi
-            let oddreal = oi + ohi
-            let oddim = ohr - or
-            
-            let _r = oddreal * _cos1 - oddim * _sin1
-            let _i = oddreal * _sin1 + oddim * _cos1
-            
-            op_r.pointee = 0.5 * (evenreal + _r)
-            op_i.pointee = 0.5 * (_i + evenim)
-            oph_r.pointee = 0.5 * (evenreal - _r)
-            oph_i.pointee = 0.5 * (_i - evenim)
-            
-            let _c1 = _cos * _cos1 - _sin * _sin1
-            let _s1 = _cos * _sin1 + _sin * _cos1
-            _cos1 = _c1
-            _sin1 = _s1
-        }
+        vDSP_fft_zrop_forward_twiddling_imp(log2N, out_real, out_imag, out_stride)
     }
 }
 @inlinable
@@ -199,6 +205,49 @@ func vDSP_fft_zrop_inverse_imp<T: Real & BinaryFloatingPoint>(_ log2N: Int, _ in
         
         vDSP_fft_zip(log2N - 1, out_real, out_imag, out_stride, .inverse)
     }
+}
+
+/// Performs a Real to Complex out-of-place discrete fourier transform. This function only result/required the first half of frequency domain.
+///
+/// - parameters:
+///   - log2N: The base 2 exponent of the number of elements to process.
+///   - real:
+///   - imag:
+///   - stride: Stride between elements in `real` and `imag`.
+///   - direction: Forward or inverse directional.
+@inlinable
+@inline(__always)
+public func vDSP_fft_zrip<T: Real & BinaryFloatingPoint>(_ log2N: Int, _ real: UnsafeMutablePointer<T>, _ imag: UnsafeMutablePointer<T>, _ stride: Int, _ direction: FFTDirection) {
+    switch direction {
+    case .forward: vDSP_fft_zrip_forward_imp(log2N, real, imag, stride)
+    case .inverse: vDSP_fft_zrip_inverse_imp(log2N, real, imag, stride)
+    }
+}
+
+@inlinable
+@inline(__always)
+func vDSP_fft_zrip_forward_imp<T: Real & BinaryFloatingPoint>(_ log2N: Int, _ real: UnsafeMutablePointer<T>, _ imag: UnsafeMutablePointer<T>, _ stride: Int) {
+    
+    switch log2N {
+        
+    case 0: break
+        
+    case 1:
+        vDSP_fft_zrop_forward_2(real, imag, real, imag)
+    case 2:
+        vDSP_fft_zrop_forward_4(real, imag, stride, real, imag, stride)
+    case 3:
+        vDSP_fft_zrop_forward_8(real, imag, stride, real, imag, stride)
+        
+    default:
+        vDSP_fft_zip_imp(log2N - 1, real, imag, stride)
+        vDSP_fft_zrop_forward_twiddling_imp(log2N, real, imag, stride)
+    }
+}
+@inlinable
+@inline(__always)
+func vDSP_fft_zrip_inverse_imp<T: Real & BinaryFloatingPoint>(_ log2N: Int, _ real: UnsafeMutablePointer<T>, _ imag: UnsafeMutablePointer<T>, _ stride: Int) {
+    vDSP_fft_zrop_inverse_imp(log2N, real, imag, stride, real, imag, stride)
 }
 
 /// Performs a Complex to Complex out-of-place discrete fourier transform.
@@ -289,6 +338,14 @@ func vDSP_fft_zop_imp<T: Real & BinaryFloatingPoint>(_ log2N: Int, _ in_real: Un
 ///   - direction: Forward or inverse directional.
 @_transparent
 public func vDSP_fft_zip<T: Real & BinaryFloatingPoint>(_ log2N: Int, _ real: UnsafeMutablePointer<T>, _ imag: UnsafeMutablePointer<T>, _ stride: Int, _ direction: FFTDirection) {
+    switch direction {
+    case .forward: vDSP_fft_zip_imp(log2N, real, imag, stride)
+    case .inverse: vDSP_fft_zip_imp(log2N, imag, real, stride)
+    }
+}
+
+@inlinable
+func vDSP_fft_zip_imp<T: Real & BinaryFloatingPoint>(_ log2N: Int, _ real: UnsafeMutablePointer<T>, _ imag: UnsafeMutablePointer<T>, _ stride: Int) {
     
     fatalError("Unimplemented.")
 }
