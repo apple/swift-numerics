f1bf57a071619d5e003df5551baeff8057f711bd
Susan Cheng <>
2019-11-09 10:53:26 +0800

    replace the license

--- Sources/Accelerate/Implementation/vDSP_fft_zrop_8.swift
+++ Sources/Accelerate/Implementation/vDSP_fft_zrop_8.swift
@@ -1,27 +1,13 @@
+//===--- vDSP_fft_zrop_8.swift ----------------------------------------*- swift -*-===//
 //
-//  vDSP_fft_zrop_8.swift
+// This source file is part of the Swift Numerics open source project
 //
-//  The MIT License
-//  Copyright (c) 2015 - 2019 Susan Cheng. All rights reserved.
+// Copyright (c) 2019 Apple Inc. and the Swift Numerics project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
+// See https://swift.org/LICENSE.txt for license information
 //
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
+//===----------------------------------------------------------------------===//
 
 @inlinable
 @inline(__always)
