1861e3d853d179685e85331985096a12d17665c1
Dan Kogai <dankogai+github@gmail.com>
2020-07-10 12:48:02 +0900

    add Complex+ElementaryFunctions.swift

---
+++ Sources/ComplexModule/Complex+ElementaryFunctions.swift
@@ -0,0 +1,90 @@
+//
+//  Complex+ElementaryFunctions.swift
+//  ComplexModule
+//
+//  Created by Dan Kogai on 2020/07/10.
+//
+
+import RealModule
+
+extension Complex:ElementaryFunctions {
+    public static func exp(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func expMinusOne(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func cosh(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func sinh(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func tanh(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func cos(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func sin(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func tan(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func log(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func log(onePlus x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func acosh(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func asinh(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func atanh(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func acos(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func asin(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func atan(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func pow(_ x: Complex<RealType>, _ y: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func pow(_ x: Complex<RealType>, _ n: Int) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func sqrt(_ x: Complex<RealType>) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+    
+    public static func root(_ x: Complex<RealType>, _ n: Int) -> Complex<RealType> {
+        fatalError("Unimplemented")
+    }
+}
