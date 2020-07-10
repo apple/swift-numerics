3bed5fa8742715ce4b33868872e1fe6b31f3e02b
Dan Kogai <dankogai+github@gmail.com>
2020-07-10 14:13:49 +0900

    ported implementations from https://github.com/dankogai/swift-complex

--- Sources/ComplexModule/Complex+ElementaryFunctions.swift
+++ Sources/ComplexModule/Complex+ElementaryFunctions.swift
@@ -1,90 +1,85 @@
+//===--- Complex+ElementaryFunctions --------------------------*- swift -*-===//
 //
-//  Complex+ElementaryFunctions.swift
-//  ComplexModule
+// This source file is part of the Swift Numerics open source project
 //
-//  Created by Dan Kogai on 2020/07/10.
+// Copyright (c) 2019 Apple Inc. and the Swift Numerics project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
 
 import RealModule
 
 extension Complex:ElementaryFunctions {
-    public static func exp(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func exp(_ z: Self) -> Self {
+        let r = RealType.exp(z.real)
+        let a = z.imaginary
+        return Self(r * .cos(a), r * .sin(a))
     }
-    
-    public static func expMinusOne(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func expMinusOne(_ z: Self) -> Self {
+        return -exp(z/2) * 2 * sin(z * .i / 2) * .i
     }
-    
-    public static func cosh(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func cosh(_ z: Self) -> Self {
+        return cos(z * .i)
     }
-    
-    public static func sinh(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func sinh(_ z: Self) -> Self {
+        return -sin(z * .i) * .i
     }
-    
-    public static func tanh(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func tanh(_ z: Self) -> Self {
+        return sinh(z) / cosh(z)
     }
-    
-    public static func cos(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func cos(_ z: Self) -> Self {
+        let (x, y) = (z.real, z.imaginary)
+        return Self(+.cos(x) * .cosh(y), -.sin(x) * .sinh(y))
     }
-    
-    public static func sin(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func sin(_ z: Self) -> Self {
+        let (x, y) = (z.real, z.imaginary)
+        return Self(+.sin(x) * .cosh(y), +.cos(x) * .sinh(y))
     }
-    
-    public static func tan(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func tan(_ z: Self) -> Self {
+        return sin(z) / cos(z)
     }
-    
-    public static func log(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func log(_ z: Self) -> Self {
+        let (r, a) = z.polar
+        return Self(RealType.log(r), a)
     }
-    
-    public static func log(onePlus x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func log(onePlus z: Self) -> Self {
+        return 2 * atanh(z / (z + 2))
     }
-    
-    public static func acosh(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func acosh(_ z:Self) -> Self {
+        return log(z + sqrt(z + 1) * sqrt(z - 1))
     }
-    
-    public static func asinh(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func asinh(_ z:Self) -> Self {
+        return log(z + sqrt(z * z + 1))
     }
-    
-    public static func atanh(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func atanh(_ z:Self) -> Self {
+        return (log(1 + z) - log(1 - z)) / 2
     }
-    
-    public static func acos(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func acos(_ z:Self) -> Self {
+         return log(z - sqrt(1 - z * z) * .i) * .i
     }
-    
-    public static func asin(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func asin(_ z:Self) -> Self {
+        return -log(z * .i + sqrt(1 - z * z)) * .i
     }
-    
-    public static func atan(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func atan(_ z:Self) -> Self {
+        return (log(1 - z * .i) - log(1 + z * .i)) * .i / 2
     }
-    
-    public static func pow(_ x: Complex<RealType>, _ y: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func pow(_ lhs: Self, _ rhs: Self) -> Self {
+        return exp(log(lhs) * rhs)
     }
-    
-    public static func pow(_ x: Complex<RealType>, _ n: Int) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func pow(_ lhs: Self, _ rhs: Int) -> Self {
+        // MARK: - NOT OPTIMAL
+        return pow(lhs, Self(RealType(rhs), 0))
     }
-    
-    public static func sqrt(_ x: Complex<RealType>) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func sqrt(_ z:Self) -> Self {
+        let r = z.length
+        let x = RealType.sqrt((r + z.real)/2)
+        let y = RealType.sqrt((r - z.real)/2)
+        return Self(x, z.imaginary.sign == .minus ? -y : y)
     }
-    
-    public static func root(_ x: Complex<RealType>, _ n: Int) -> Complex<RealType> {
-        fatalError("Unimplemented")
+    public static func root(_ z: Self, _ n: Int) -> Self {
+        // MARK: - NOT OPTIMAL
+        return pow(z, Self(RealType(1) / RealType(n)))
     }
 }
