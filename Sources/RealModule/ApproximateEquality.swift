e69f12617b2d717149c625ad90f96a7ed09bac0a
Stephen Canon <scanon@apple.com>
2020-07-16 14:35:21 -0400

    Lift the most general isApproximatelyEqual(to:) to AdditiveArithmetic.
    The most general version of the API is now defined on AA as:
    
      ```
      public func isApproximatelyEqual<Magnitude>(
        to other: Self,
        absoluteTolerance: Magnitude,
        relativeTolerance: Magnitude = 0,
        norm: (Self) -> Magnitude
      ) -> Bool where Magnitude: FloatingPoint {
      ```
    
    Note that AA does not have the magnitude property, so we can't default the norm in this setting. Therefore the following is kept on Numeric:
    
      ```
      public func isApproximatelyEqual(
        to other: Self,
        absoluteTolerance: Magnitude,
        relativeTolerance: Magnitude = 0
      ) -> Bool {
        self.isApproximatelyEqual(
          to: other,
          absoluteTolerance: absoluteTolerance,
          relativeTolerance: relativeTolerance,
          norm: \.magnitude
        )
      }
      ```
    
    to provide the operation with norm defaulted (note: it's not optional, because then this would call itself).

--- Sources/RealModule/ApproximateEquality.swift
+++ Sources/RealModule/ApproximateEquality.swift
@@ -10,16 +10,15 @@
 //===----------------------------------------------------------------------===//
 
 extension Numeric where Magnitude: FloatingPoint {
-  /// Compares `self` and `other` for approximate equality with a specified
-  /// relative tolerance and implicit absolute tolerance.
+  /// Test if `self` and `other` are approximately equal.
   ///
   /// `true` if `self` and `other` are equal, or if they are finite and
   /// ```
-  /// (self - other).magnitude ≤ relativeTolerance * scale
+  /// norm(self - other) <= relativeTolerance * scale
   /// ```
   /// where `scale` is
   /// ```
-  /// max(self.magnitude, other.magnitude, .leastNormalMagnitude)
+  /// max(norm(self), norm(other), .leastNormalMagnitude)
   /// ```
   ///
   /// The default value of `relativeTolerance` is `.ulpOfOne.squareRoot()`,
@@ -28,22 +27,27 @@ extension Numeric where Magnitude: FloatingPoint {
   /// computation being performed, but is not suitable for all use cases.
   ///
   /// Mathematical Properties:
-  /// -
-  /// - `isApproximatelyEqual(to:relativeTolerance:)` is _reflexive_ for
+  /// ------------------------
+  /// 
+  /// - `isApproximatelyEqual(to:relativeTolerance:norm:)` is _reflexive_ for
   ///   non-exceptional values (such as NaN).
   ///
-  /// - `isApproximatelyEqual(to:relativeTolerance:)` is _symmetric_.
+  /// - `isApproximatelyEqual(to:relativeTolerance:norm:)` is _symmetric_.
   ///
-  /// - `isApproximatelyEqual(to:relativeTolerance:)` is __not__ _transitive_.
+  /// - `isApproximatelyEqual(to:relativeTolerance:norm:)` is __not__ _transitive_.
   ///   Because of this, approximately equality is __not an equivalence relation__,
   ///   even when restricted to non-exceptional values.
   ///
   /// - For any point `a`, the set of values that compare approximately equal to `a` is _convex_.
   ///   (Under the assumption that the `.magnitude` property implements a valid norm.)
   ///
-  /// - `isApproximatelyEqual(to:relativeTolerance:)` is _scale invariant_,
+  /// - `isApproximatelyEqual(to:relativeTolerance:norm:)` is _scale invariant_,
   ///   so long as no underflow or overflow has occured, and no exceptional value is produced
   ///   by the scaling.
+  ///
+  /// See Also:
+  /// -------
+  /// - `isApproximatelyEqual(to:absoluteTolerance:[relativeTolerance:norm:])`
   ///
   /// - Parameters:
   ///
@@ -55,32 +59,40 @@ extension Numeric where Magnitude: FloatingPoint {
   ///     This value should be non-negative and less than or equal to 1.
   ///     This constraint on is only checked in debug builds, because a mathematically
   ///     well-defined result exists for any tolerance, even one out of range.
+  ///
+  ///   - norm: The [norm] to use for the comparison.
+  ///     Defaults to `\.magnitude`.
+  ///
+  /// [norm]: https://en.wikipedia.org/wiki/Norm_(mathematics)
   @inlinable @inline(__always)
   public func isApproximatelyEqual(
     to other: Self,
-    relativeTolerance: Magnitude = Magnitude.ulpOfOne.squareRoot()
+    relativeTolerance: Magnitude = Magnitude.ulpOfOne.squareRoot(),
+    norm: (Self) -> Magnitude = \.magnitude
   ) -> Bool {
     return isApproximatelyEqual(
       to: other,
       absoluteTolerance: relativeTolerance * Magnitude.leastNormalMagnitude,
-      relativeTolerance: relativeTolerance
+      relativeTolerance: relativeTolerance,
+      norm: norm
     )
   }
   
-  /// Compares `self` and `other` for approximate equality with specified tolerances.
+  /// Test if `self` and `other` are approximately equal with specified tolerances.
   ///
   /// `true` if `self` and `other` are equal, or if they are finite and either
   /// ```
-  /// (self - other).magnitude ≤ absoluteTolerance
+  /// (self - other).magnitude <= absoluteTolerance
   /// ```
   /// or
   /// ```
-  /// (self - other).magnitude ≤ relativeTolerance * scale
+  /// (self - other).magnitude <= relativeTolerance * scale
   /// ```
   /// where `scale` is `max(self.magnitude, other.magnitude)`.
   ///
   /// Mathematical Properties:
-  /// -
+  /// ------------------------
+  ///
   /// - `isApproximatelyEqual(to:absoluteTolerance:relativeTolerance:)`
   ///   is _reflexive_ for non-exceptional values (such as NaN).
   ///
@@ -94,6 +106,76 @@ extension Numeric where Magnitude: FloatingPoint {
   /// - For any point `a`, the set of values that compare approximately equal to `a` is _convex_.
   ///   (Under the assumption that `norm` implements a valid norm, which cannot be checked
   ///   by this function.)
+  ///
+  /// See Also:
+  /// -------
+  /// - `isApproximatelyEqual(to:[relativeTolerance:])`
+  ///
+  /// - Parameters:
+  ///
+  ///   - other: The value to which `self` is compared.
+  ///
+  ///   - absoluteTolerance: The absolute tolerance to use in the comparison.
+  ///
+  ///     This value should be non-negative and finite.
+  ///     This constraint on is only checked in debug builds, because a mathematically
+  ///     well-defined result exists for any tolerance, even one out of range.
+  ///
+  ///   - relativeTolerance: The relative tolerance to use in the comparison.
+  ///     Defaults to zero.
+  ///
+  ///     This value should be non-negative and less than or equal to 1.
+  ///     This constraint on is only checked in debug builds, because a mathematically
+  ///     well-defined result exists for any tolerance, even one out of range.
+  @inlinable @inline(__always)
+  public func isApproximatelyEqual(
+    to other: Self,
+    absoluteTolerance: Magnitude,
+    relativeTolerance: Magnitude = 0
+  ) -> Bool {
+    self.isApproximatelyEqual(
+      to: other,
+      absoluteTolerance: absoluteTolerance,
+      relativeTolerance: relativeTolerance,
+      norm: \.magnitude
+    )
+  }
+}
+
+extension AdditiveArithmetic {
+  /// Test if `self` and `other` are approximately equal with specified tolerances and norm.
+  ///
+  /// `true` if `self` and `other` are equal, or if they are finite and either
+  /// ```
+  /// norm(self - other) <= absoluteTolerance
+  /// ```
+  /// or
+  /// ```
+  /// norm(self - other) <= relativeTolerance * scale
+  /// ```
+  /// where `scale` is `max(norm(self), norm(other))`.
+  ///
+  /// Mathematical Properties:
+  /// ------------------------
+  ///
+  /// - `isApproximatelyEqual(to:absoluteTolerance:relativeTolerance:norm:)`
+  ///   is _reflexive_ for non-exceptional values (such as NaN).
+  ///
+  /// - `isApproximatelyEqual(to:absoluteTolerance:relativeTolerance:norm:)`
+  ///   is _symmetric_.
+  ///
+  /// - `isApproximatelyEqual(to:absoluteTolerance:relativeTolerance:norm:)`
+  ///   is __not__ _transitive_. Because of this, approximately equality is
+  ///   __not an equivalence relation__, even when restricted to non-exceptional values.
+  ///
+  /// - For any point `a`, the set of values that compare approximately equal to `a` is _convex_.
+  ///   (Under the assumption that `norm` implements a valid norm, which cannot be checked
+  ///   by this function.)
+  ///
+  /// See Also:
+  /// -------
+  /// - `isApproximatelyEqual(to:[relativeTolerance:norm:])`
+  /// - `isApproximatelyEqual(to:absoluteTolerance:[relativeTolerance:])`
   ///
   /// - Parameters:
   ///
@@ -112,13 +194,11 @@ extension Numeric where Magnitude: FloatingPoint {
   ///     This constraint on is only checked in debug builds, because a mathematically
   ///     well-defined result exists for any tolerance, even one out of range.
   ///
-  ///   - norm: The norm to use for the comparison. Defaults to the `.magnitude`
-  ///     property.
+  ///   - norm: The norm to use for the comparison.
+  ///     Defaults to `\.magnitude`.
   ///
-  ///     When porting code or working with specialized algorithms, it may
-  ///     be desirable to override the norm. For example, if we wanted to test if
-  ///     a complex value was inside a circle of radius 0.001 centered at (1 + 0i), we
-  ///     could use:
+  ///     For example, if we wanted to test if a complex value was inside a circle of
+  ///     radius 0.001 centered at (1 + 0i), we could use:
   ///     ```
   ///     z.isApproximatelyEqual(
   ///       to: 1,
@@ -128,13 +208,20 @@ extension Numeric where Magnitude: FloatingPoint {
   ///     ```
   ///     (if we used the default, we would be testing if `z` were inside a square region
   ///     instead.)
-  @inlinable @inline(__always)
-  public func isApproximatelyEqual(
+  @inlinable
+  public func isApproximatelyEqual<Magnitude>(
     to other: Self,
     absoluteTolerance: Magnitude,
     relativeTolerance: Magnitude = 0,
-    norm: (Self) -> Magnitude = { $0.magnitude }
-  ) -> Bool {
+    norm: (Self) -> Magnitude
+  ) -> Bool
+  // TODO: constraint should really be weaker than FloatingPoint,
+  // but we need to have `isFinite` for it to work correctly with
+  // floating-point magnitudes in generic contexts, which is the
+  // most common case. The fix for this is to lift the isFinite
+  // requirement to Numeric in the standard library, but that's
+  // source-breaking, so requires an ABI rumspringa.
+  where Magnitude: FloatingPoint {
     assert(
       absoluteTolerance >= 0 && absoluteTolerance.isFinite,
       "absoluteTolerance should be non-negative and finite, " +
