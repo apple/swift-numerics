337be4044e9db2b418098f3177fcc4334a50dfce
Stephen Canon <scanon@apple.com>
2020-08-23 15:58:48 -0400

    Rename some tests to reflect what they actually do.

--- Tests/RealTests/ApproximateEqualityTests.swift
+++ Tests/RealTests/ApproximateEqualityTests.swift
@@ -1,7 +1,19 @@
+//===--- ApproximateEqualityTests.swift -----------------------*- swift -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2020 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
 import RealModule
 import XCTest
 
-final class ElementaryFunctionTests: XCTestCase {
+final class ApproximateEqualityTests: XCTestCase {
   
   func testSpecials<T: Real>(absolute tol: T) {
     let zero = T.zero
