6dff4a68958c28751b329a72ad7bb227d2d54457
Markus Winter <markus@wntr.me>
2022-05-02 17:29:40 +0200

    Seperate test helper from quaternion module

--- Sources/QuaternionModule/ImaginaryHelper.swift
+++ Sources/QuaternionModule/ImaginaryHelper.swift
@@ -19,18 +19,6 @@ extension SIMD3 where Scalar: FloatingPoint {
     SIMD3(repeating: .infinity)
   }
 
-  /// Returns a vector with nan in all lanes
-  @usableFromInline @inline(__always)
-  internal static var nan: Self {
-    SIMD3(repeating: .nan)
-  }
-
-  /// Returns a vector with .ulpOfOne in all lanes
-  @usableFromInline @inline(__always)
-  internal static var ulpOfOne: Self {
-    SIMD3(repeating: .ulpOfOne)
-  }
-
   /// True if all values of this instance are finite
   @usableFromInline @inline(__always)
   internal var isFinite: Bool {
