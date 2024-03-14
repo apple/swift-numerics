86000c159699d27f5331f6576ed23b2b36ad3e31
Markus Winter <markus@wntr.me>
2024-03-14 13:32:53 +0100

    [Tests] Update copyright notices

--- Tests/Executable/ComplexLog/main.swift
+++ Tests/Executable/ComplexLog/main.swift
@@ -1,12 +1,11 @@
 //===--- main.swift -------------------------------------------*- swift -*-===//
 //
-// This source file is part of the Swift.org open source project
+// This source file is part of the Swift Numerics open source project
 //
-// Copyright (c) 2020 Apple Inc. and the Swift project authors
+// Copyright (c) 2020 - 2024 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 //===----------------------------------------------------------------------===//
 
--- Tests/IntegerUtilitiesTests/RotateTests.swift
+++ Tests/IntegerUtilitiesTests/RotateTests.swift
@@ -1,12 +1,11 @@
 //===--- RotateTests.swift ------------------------------------*- swift -*-===//
 //
-// This source file is part of the Swift.org open source project
+// This source file is part of the Swift Numerics open source project
 //
-// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Copyright (c) 2021 - 2024 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 //===----------------------------------------------------------------------===//
 
