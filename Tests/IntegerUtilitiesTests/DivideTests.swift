da661919b91a8ca16572b7dc66951bd034e254f8
Stephen Canon <scanon@apple.com>
2024-07-01 12:46:42 -0400

    Fixup some file headers that had the Swift.org text instead of Swift Numerics
    These are a copy-pasto from introduced when I originally imported approximate equality (which was originally proposed for the standard library).

--- Tests/IntegerUtilitiesTests/DivideTests.swift
+++ Tests/IntegerUtilitiesTests/DivideTests.swift
@@ -1,8 +1,8 @@
 //===--- DivideTests.swift ------------------------------------*- swift -*-===//
 //
-// This source file is part of the Swift.org open source project
+// This source file is part of the Swift Numerics open source project
 //
-// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Copyright (c) 2021 Apple Inc. and the Swift Numerics project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information
