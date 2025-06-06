3a16fabfb31aa19051c887b1281f105d553c26a3
Jason Bobier <jason@prismatix.com>
2025-06-06 10:00:54 +0200

    Fixed bad merge.

--- Tests/IntegerUtilitiesTests/GreatestCommonDivisorTests.swift
+++ Tests/IntegerUtilitiesTests/GreatestCommonDivisorTests.swift
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 //===--- GreatestCommonDivisorTests.swift ---------------------------------------*- swift -*-===//
-=======
-//===--- GCDTests.swift ---------------------------------------*- swift -*-===//
->>>>>>> 1660f6bdd8fe76f9e6d7157380512059f88e3686
 //
 // This source file is part of the Swift Numerics open source project
 //
@@ -17,15 +13,9 @@
 import IntegerUtilities
 import Testing
 
-<<<<<<< HEAD
 @Suite("Greatest Common Divisor Tests")
 struct GreatestCommonDivisorTests {
 	@Test("gcd") func gcdTests() async throws {
-=======
-@Suite("GCD Tests")
-struct GCDTests {
-	@Test("gcd Tests") func gcdTests() async throws {
->>>>>>> 1660f6bdd8fe76f9e6d7157380512059f88e3686
 		#expect(gcd(0, 0) == 0)
 		#expect(gcd(0, 1) == 1)
 		#expect(gcd(1, 0) == 1)
@@ -47,54 +37,8 @@ struct GCDTests {
 		#expect(gcd(97*67*53*27*8, 83*67*53*9*32) == 67*53*9*8)
 		#expect(gcd(Int.min, 2) == 2)
 		#expect(gcd(Int.max, Int.max) == Int.max)
-<<<<<<< HEAD
 		#expect(gcd(0, Int.min) == Int.min.magnitude)
 		#expect(gcd(Int.min, 0) == Int.min.magnitude)
 		#expect(gcd(Int.min, Int.min) == Int.min.magnitude)
-=======
-
-//		#expect(processExitsWith: .failure) {
-//			gcd(0, Int.min)
-//		}
-//		#expect(processExitsWith: .failure) {
-//			gcd(Int.min, 0)
-//		}
-//		#expect(processExitsWith: .failure) {
-//			gcd(Int.min, Int.min)
-//		}
-	}
-
-	@Test("greatestCommonDivisor Tests") func greatestCommonDivisorTests() async throws {
-		#expect(try greatestCommonDivisor(0, 0) == 0)
-		#expect(try greatestCommonDivisor(0, 1) == 1)
-		#expect(try greatestCommonDivisor(1, 0) == 1)
-		#expect(try greatestCommonDivisor(0, -1) == 1)
-		#expect(try greatestCommonDivisor(-1, 0) == 1)
-		#expect(try greatestCommonDivisor(1, 1) == 1)
-		#expect(try greatestCommonDivisor(1, 2) == 1)
-		#expect(try greatestCommonDivisor(2, 2) == 2)
-		#expect(try greatestCommonDivisor(4, 2) == 2)
-		#expect(try greatestCommonDivisor(6, 8) == 2)
-		#expect(try greatestCommonDivisor(77, 91) == 7)
-		#expect(try greatestCommonDivisor(24, -36) == 12)
-		#expect(try greatestCommonDivisor(-24, -36) == 12)
-		#expect(try greatestCommonDivisor(51, 34) == 17)
-		#expect(try greatestCommonDivisor(64, 96) == 32)
-		#expect(try greatestCommonDivisor(-64, 96) == 32)
-		#expect(try greatestCommonDivisor(4*7*19, 27*25) == 1)
-		#expect(try greatestCommonDivisor(16*315, 11*315) == 315)
-		#expect(try greatestCommonDivisor(97*67*53*27*8, 83*67*53*9*32) == 67*53*9*8)
-		#expect(try greatestCommonDivisor(Int.min, 2) == 2)
-		#expect(try greatestCommonDivisor(Int.max, Int.max) == Int.max)
-		#expect(throws: OverflowError(value: Int.min.magnitude)) {
-			try greatestCommonDivisor(0, Int.min)
-		}
-		#expect(throws: OverflowError(value: Int.min.magnitude)) {
-			try greatestCommonDivisor(Int.min, 0)
-		}
-		#expect(throws: OverflowError(value: Int.min.magnitude)) {
-			try greatestCommonDivisor(Int.min, Int.min)
-		}
->>>>>>> 1660f6bdd8fe76f9e6d7157380512059f88e3686
 	}
 }
