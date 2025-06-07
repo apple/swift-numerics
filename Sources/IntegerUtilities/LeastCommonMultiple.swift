9d5e5021cb6f039d28b8716737449f458a108dbe
Jason Bobier <jason@prismatix.com>
2025-06-07 13:09:15 +0200

    Cleaned up documentation and comments.

--- Sources/IntegerUtilities/LeastCommonMultiple.swift
+++ Sources/IntegerUtilities/LeastCommonMultiple.swift
@@ -46,7 +46,7 @@ public func lcm<T: FixedWidthInteger>(_ a: T, _ b: T) throws(LeastCommonMultiple
 
 	let reduced = a.magnitude / gcd(a, b)
 
-	// We optimize for the non-throwing case
+	// We could use the multipliedFullWidth directly here, but we optimize instead for the non-throwing case because multipliedReportingOverflow is much faster.
 	let (partialValue, overflow) = reduced.multipliedReportingOverflow(by: b.magnitude)
 
 	guard !overflow, let result = T(exactly: partialValue) else {
@@ -61,13 +61,15 @@ public func lcm<T: FixedWidthInteger>(_ a: T, _ b: T) throws(LeastCommonMultiple
 
 /// Error thrown by `lcm<FixedWidthInteger>`.
 ///
-/// `high` and `low` can be combined into a double width integer to access the result.
+/// Thrown when the result of the lcm isn't representable within its type. You can combine `high` and `low` into a double width integer to access the result.
 ///
-/// For example a `LeastCommonMultipleOverflowError<Int8>` contains the result as `Int16(error.high) << 8 | Int16(error.low)`
+/// For example a `LeastCommonMultipleOverflowError<Int8>` has `UInt8` as its Magnitude and contains the result in `high: UInt8` and `low: UInt8`.
+/// These can be combined into a UInt16 result as `UInt16(high) << 8 | UInt16(low)`.
 public struct LeastCommonMultipleOverflowError<T: FixedWidthInteger>: Error, Equatable {
 	public let high: T.Magnitude
 	public let low: T.Magnitude
 
+	@inlinable
 	public init(high: T.Magnitude, low: T.Magnitude) {
 		self.high = high
 		self.low = low
